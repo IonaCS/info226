3f338b02bc4ee9f3cef4e8585dacc6e4d920892d
tylereagle <tyler.a.eagle@gmail.com>
2019-05-15 11:14:13 +1200

    reading from server for roads
    now able to display information from the roads directory json file

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ app.controller('mainCtrl', function($scope, $http) {
 		Check login details correct
 	****************************************************/
 	$scope.checkLogin = function() {
-		var user_list = "https://track.sim.vuw.ac.nz/api/sammoniona/user_list.json"
+		var user_list = "https://track.sim.vuw.ac.nz/api/sammoniona/user_list.json";
 		$http.get(user_list).then (function (response) {
 			//Iterate through server list of correct logins,
 			//Then compare with username/password entered:
@@ -35,14 +35,26 @@ app.controller('mainCtrl', function($scope, $http) {
 	};
 
 	$scope.showRoadList = function() {
-		// IF list of roads hidden, show. 
+		// IF list of roads hidden, show.
 		// ELSE IF list visible, hide:
-		if ($scope.roadsList == false) {
-			$scope.roadsList = true;
-		} else if ($scope.roadsList == true) {
-			$scope.roadsList = false;
-		}
+  	var road_dir = "https://track.sim.vuw.ac.nz/api/eagletyle/road_dir.json";
+    $http.get(road_dir).then (function (response) {
+			//Iterate through server list of correct logins,
+			//Then compare with username/password entered:
+      num_of_users = response.data.Roads.length;
+      for (var i = 0; i < num_of_users; i++) {
+        if ($scope.roadsList == false) {
+          $scope.roadsList = true;
+          $scope.ID = response.data.Roads[i].ID;
+          $scope.roadName = response.data.Roads[i].Code;
+          $scope.roadType = response.data.Roads[i].Type;
+          $scope.section = response.data.Roads[i].Section;
+          $scope.location = response.data.Roads[i].Location;
+          $scope.latLon = response.data.Roads[i].GPS;
+        } else if ($scope.roadsList == true) {
+          $scope.roadsList = false;
+        }
+      };
+		});
 	};
-
-
 });
